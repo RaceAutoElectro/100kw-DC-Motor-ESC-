10385119c1fe2a05120f7d2b7c70e8958dd6a268
Konstantin Kavruk <6k6k6k@gmail.com>
2016-05-18 12:46:22 +0300

    Update _100kw_esc.ino

--- Arduino/_100kw_esc/_100kw_esc.ino
+++ Arduino/_100kw_esc/_100kw_esc.ino
@@ -1,44 +1,54 @@
-/*
-  pin0,1 -Serial COM
-  pin2 - int-can (and pin10,11,12,13)
-  pin3 - buton1
-  pin5 - pwm - rotor 1300 fet amp
-  pin6 - pwm led
-  pin7 - buton2
-  pin8 - precharge ralay
-  pin9 - pwm - stator 260 fet amp
-
-  ----------------------------------
-
-  pinA0 - amperage pin          519
-  pinA1 - lm35 temp senzor             //
-  pinA2 - fb2                947 - 562
-  pinA3 - fb1                157 - 940
-  pinA4 - SDA                   LCD
-  pinA5 - SCL                   LCD
-  pinA6- volt-HV 100k & 2.2k
-  pinA7- volt-lv 10k & 2.2k
-
-
-  12.44 - 460
-  13.78 - 507
-
-
-  11.77-50
-  11.56-49
-  11.52-48
+//
+//  pin0,1 -Serial COM
+//  pin2 - int-can (and pin10,11,12,13)
+//  pin3 - buton1
+//  pin7 - buton2
+//  pin8 - precharge ralay
+//  pin9 - pwm - stator 260 fet amp
+//
+//  ----------------------------------
+//  pinA2 - fb2                947 - 562
+//  pinA3 - fb1                157 - 940
+//  pinA4 - SDA                   LCD
+//  pinA5 - SCL                   LCD
+//  pinA6- volt-HV 100k & 2.2k
+//  pinA7- volt-lv 10k & 2.2k
+//
+//
+//  12.44 - 460
+//  13.78 - 507
+//
+//
+//  11.77-50
+//  11.56-49
+//  11.52-48
+#define SERIAL_ENABLED 1
+#define LCD_ENABLED 1
 
 
-*/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int AMPPin  = A0;  // A1302 hall senzor
-const int FB2Pin  = A2;  // FB2     947 - 562   (Bosch)
-const int FB1Pin  = A3;  // FB1     157 - 940
-const int HVPin  = A6;  // volt-HV 100k & 2.2k
-const int LVPin  = A7;  // volt-lv 10k & 2.2k
-const int pwmRot = 5;          // MainPWM for Rotor
-const int pwmStat = 9;        // SecPWM for Stator
+// A1302 hall senzor
+#define CURRENT_PIN A0
+//  pinA1 - lm35 temp senzor             //
+#define TEMP_PIN A1
+// FB2     947 - 562   (Bosch)
+#define FB2_PIN A2
+// FB1     157 - 940
+#define FB1_PIN A3
+// volt-HV 100k & 2.2k
+#define HV_PIN A6
+// MainPWM for Rotor
+#define ROTOR_PWM_PIN 6
+// SecPWM for Stator
+#define STATOR_PWM_PIN 9
+
 int MainFB = 0;               // MainFB ( TPS1 )
 int SecFB = 0;                // SecFB ( TPS2 )
 int LoVoltage = 0;           // Low Voltage 0-22v
@@ -49,29 +59,35 @@ int tempValue = 0;           //  lm35 -temperature senzor
 int AMP = 0;                //  A1302 hall senzor
 
 void setup() {
+#if SERIAL
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+#endif
+#if LCD
+  // initialize the LCD
+  lcd.begin(20, 4, LCD_5x8DOTS);
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+  lcd.print("Hello, world1!");
+#endif
 }
 
 void loop() {
   // read the analog in value:
   tempValue = (5.0 * analogRead(A1) * 100.0) / 1024;
-  AMP = (analogRead(AMPPin ) - 518) / 2;
-  MainFB = analogRead(FB1Pin );
+  AMP = (analogRead(CURRENT_PIN) - 518) / 2;
+  MainFB = analogRead(FB1_PIN );
   MainFB = constrain(MainFB, 154, 940);
-  SecFB =  analogRead(FB2Pin );
+  SecFB =  analogRead(FB2_PIN );
   SecFB = constrain(SecFB, 948, 562);
-  LoVoltage = (5.0 * analogRead(LVPin ) * 560.0) / 1024; ;
-  HiVoltage =  (5.0 * analogRead(HVPin ) * 4750.0) / 1024;
-
+  HiVoltage =  (5.0 * analogRead(HV_PIN ) * 4750.0) / 1024;
   // map it to the range of the analog out:
   MainPWM = map(MainFB, 154, 940, 0, 255);
   SecPWM = map(SecFB, 948, 562, 0, 155); //temp limit
-
   // change the analog out value:
-  analogWrite(pwmRot, MainPWM);
-  analogWrite(pwmStat, SecPWM);
-
+  analogWrite(ROTOR_PWM_PIN, MainPWM);
+  analogWrite(STATOR_PWM_PIN, SecPWM);
+#if SERIAL_ENABLED
   // print the results to the serial monitor:
   Serial.print("MainFB = ");
   Serial.print(MainFB);
@@ -79,8 +95,6 @@ void loop() {
   Serial.print(SecFB);
   Serial.print("\t HiVoltage = ");
   Serial.print(HiVoltage);
-  Serial.print("\t LoVoltage = ");
-  Serial.print(LoVoltage);
   Serial.print("\t AMP = ");
   Serial.print(AMP);
   Serial.print("\t temp = ");
@@ -90,8 +104,22 @@ void loop() {
   Serial.print("\t SecPwm = ");
   Serial.print(SecPWM);
   Serial.println( );
-
-
+#endif
+#if LCD_ENABLED
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("LoV=");
+  lcd.print(LoVoltage);
+  lcd.setCursor(9, 0);
+  lcd.print("Tc=");
+  lcd.print(tempValue);
+  lcd.setCursor(0, 1);
+  lcd.print("AMP=");
+  lcd.print(AMP);
+  lcd.setCursor(9, 1);
+  lcd.print("HV=");
+  lcd.print(HiVoltage);
+#endif
 
 
   // wait 2 milliseconds before the next loop
