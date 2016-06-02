d99241192f15b84d1d2e3182d50a9dd5b819f4ed
Konstantin Kavruk <6k6k6k@gmail.com>
2016-06-02 14:41:27 +0300

    Update _100kw_esc.ino

--- Arduino/_100kw_esc/_100kw_esc.ino
+++ Arduino/_100kw_esc/_100kw_esc.ino
@@ -1,38 +1,43 @@
-#define SERIAL_ENABLED 0
+#define SERIAL_ENABLED 1
 #define LCD_ENABLED 1
 #include <Wire.h>
 #if LCD_ENABLED
 #include <LiquidCrystal_I2C.h>
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
-  LiquidCrystal_I2C lcd(0x27, 16, 2, LCD_5x8DOTS);
-  
+LiquidCrystal_I2C lcd(0x27, 16, 2, LCD_5x8DOTS);
+
 #endif
 // These defines won't change.  They're used to give names to the pins used:
 // A1302 hall senzor
 #define CURRENT_PIN A0
-//  pinA1 - lm35 temp senzor             //
+//pinA1 - lm35 temp senzor
 #define TEMP_PIN A1
 // FB2     947 - 562   (Bosch)
-#define FB2_PIN A2
-// FB1     157 - 940
-#define FB1_PIN A3
+#define FB2_PIN A3
+// FB1     157 - 940   (Bosch)
+#define FB1_PIN A2
 // volt-HV 100k & 2.2k
 #define HV_PIN A6
-// MainPWM for Rotor
-#define ROTOR_PWM_PIN 6
-// SecPWM for Stator
-#define STATOR_PWM_PIN 9
-
+#define ROTOR_PWM_PIN 9
+#define STATOR_PWM_PIN 6
+#define MAX_AMP 30
+#define Kp 1
 int MainFB = 0;               // MainFB ( TPS1 )
 int SecFB = 0;                // SecFB ( TPS2 )
-int HiVoltage = 0;            // Hi  Voltage 0-220v
+float HiVoltage = 0;            // Hi  Voltage 0-220v
 int MainPWM = 0;              // MainPWM for Rotor
 int SecPWM = 0;               // SecPWM for Stator
 int tempValue = 0;            //  lm35 -temperature sensor
-int AMP = 0;                  //  A1302 hall senzor
+float AMP = 0, TargetAMP = 0;                  //  A1302 hall senzor
+
+
 
 void setup() {
-#if SERIAL
+  pinMode(STATOR_PWM_PIN, OUTPUT);
+  pinMode(ROTOR_PWM_PIN, OUTPUT);
+  digitalWrite(STATOR_PWM_PIN, 0);
+  digitalWrite(ROTOR_PWM_PIN, 0);
+#if SERIAL_ENABLED
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
 #endif
@@ -41,45 +46,49 @@ void setup() {
   lcd.begin();
   // Turn on the blacklight and print a message.
   lcd.backlight();
-  lcd.print("Hello, world1!");
 #endif
 }
 
 void loop() {
   // read the analog in value:
   tempValue = (5.0 * analogRead(A1) * 100.0) / 1024;
-  for(char i=0;i<10;i++)
-    AMP += (analogRead(CURRENT_PIN) - 518) / 2;
-  AMP=AMP/10;
-  
-  MainFB = analogRead(FB1_PIN );
-  MainFB = constrain(MainFB, 154, 940);
-  SecFB =  analogRead(FB2_PIN );
-  SecFB = constrain(SecFB, 948, 562);
-  HiVoltage =  (5.0 * analogRead(HV_PIN ) * 4750.0) / 1024;
+  AMP = 0;
+  HiVoltage = 0;
+  for (char i = 0; i < 30; i++) {
+    AMP += analogRead(CURRENT_PIN);
+    HiVoltage += analogRead(HV_PIN);
+  }
+  AMP = (AMP / 30 - 514.5) / 2 - 1.05;
+  //HiVoltage = (5.0 * HiVoltage/30 * 4750.0) / 2048 ;
+  HiVoltage = HiVoltage / 264.0;
+  //MainFB = analogRead(FB1_PIN );
+  //MainFB = constrain(MainFB, 154, 940);
+  MainFB =  analogRead(FB1_PIN );
+  MainFB = constrain(MainFB, 310, 760);
   // map it to the range of the analog out:
   //mai intai te uiti ca iti vine MAINFB si SECFB ca limite si bagi limitele in map
   //MainPWM = map(MainFB, 154, 940, 0, 12); //limita laPWM 4.7%
-  //SecPWM = map(SecFB, 948, 562, 0, 12);   //limita laPWM 4.7%
+  TargetAMP = map(MainFB, 310, 770, 0, MAX_AMP);   //limita la MAX_AMP
+  MainPWM = (TargetAMP - AMP) * (255 / TargetAMP) * Kp;
   // change the analog out value:
+  //analogWrite(ROTOR_PWM_PIN, MainPWM);
   analogWrite(ROTOR_PWM_PIN, MainPWM);
-  analogWrite(STATOR_PWM_PIN, SecPWM);
 #if SERIAL_ENABLED
   // print the results to the serial monitor:
-  Serial.print("MainFB = ");
-  Serial.print(MainFB);
+  //  Serial.print("MainFB = ");
+  //  Serial.print(MainFB);
   Serial.print("\t SecFB = ");
-  Serial.print(SecFB);
+  Serial.print(MainFB);
   Serial.print("\t HiVoltage = ");
   Serial.print(HiVoltage);
   Serial.print("\t AMP = ");
   Serial.print(AMP);
   Serial.print("\t temp = ");
   Serial.print(tempValue);
-  Serial.print("\t MainPwm = ");
-  Serial.print(MainPWM);
-  Serial.print("\t SecPwm = ");
-  Serial.print(SecPWM);
+  //  Serial.print("\t MainPwm = ");
+  //  Serial.print(MainPWM);
+  //  Serial.print("\t SecPwm = ");
+  //  Serial.print(SecPWM);
   Serial.println( );
 #endif
 #if LCD_ENABLED
@@ -89,7 +98,7 @@ void loop() {
   lcd.print(" A");
   lcd.setCursor(9, 0);
   lcd.print(tempValue);
-  lcd.print("ºC");
+  lcd.print(" C");
   lcd.setCursor(0, 1);
   lcd.print(HiVoltage);
   lcd.print(" V");
@@ -97,5 +106,5 @@ void loop() {
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(20);
+  delay(200);
 }
